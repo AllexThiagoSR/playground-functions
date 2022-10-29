26250f528025cacf67dd0b568e1ea600a1401607
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-10-29 13:40:03 -0300

    Resolve quase todos os porblemas de lint de challenges2.js

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,29 +1,29 @@
 // Desafio 11
-const iteratesOver = (array, start, end, ) => {
+const checkRepetitions = (array) => {
+  for (let i of array) {
+    let count = 0;
+    for (let j of array) {
+      if (i === j) {
+        count += 1;
+      }
+    }
+    if (count >= 3) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const iteratesOver = (array, start, end) => {
   let returnString = '';
 
-  for(let index = start; index <= end; index += 1){
-    if(array[index] < 0 || array[index] > 9 || checkRepetitions(array)){
+  for (let index = start; index <= end; index += 1) {
+    if (array[index] < 0 || array[index] > 9 || checkRepetitions(array)) {
       return true;
     }
     returnString += array[index];
   }
   return returnString;
-};
-
-const checkRepetitions = (array) => {
-  for (let i in array) {
-    let count = 0;
-    for (let j in array) {
-      if(array[i] === array[j]){
-        count += 1;
-      }
-    }
-    if(count >= 3){
-      return true;
-    }
-  }
-  return false;
 };
 
 const generatePhoneNumber = (phoneArray) => {
@@ -32,7 +32,8 @@ const generatePhoneNumber = (phoneArray) => {
 
   if (phoneArray.length !== 11) {
     return 'Array com tamanho incorreto.';
-  } else if (check === true){
+  }
+  if (check === true) {
     return 'não é possível gerar um número de telefone com esses valores';
   }
   returnString += iteratesOver(phoneArray, 2, 6);
@@ -42,8 +43,9 @@ const generatePhoneNumber = (phoneArray) => {
 };
 
 // Desafio 12
-const checkFirstCondition = (a ,b ,c) => {
+const checkFirstCondition = (a, b, c) => {
   const returnArray = [];
+
   returnArray.push((a < b + c) && (a > Math.abs(b - c)));
   returnArray.push((b < a + c) && (b > Math.abs(a - c)));
   returnArray.push((c < a + b) && (c > Math.abs(a - b)));
@@ -60,28 +62,26 @@ const triangleCheck = (lineA, lineB, lineC) => {
 };
 
 // Desafio 13
-const hydrate = (str) => {
-  let cupsOfWater = 0;
-
-  for(let value of numbersInString(str)){
-    cupsOfWater += value;
-  }
-  return cupsOfWater > 1 ? `${cupsOfWater} copos de água` : `${cupsOfWater} copo de água`;
-};
-
 const numbersInString = (str) => {
-  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  // const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const nAn = 'NaN';
   const stringNumbers = [];
 
   for (let word of str.split(' ')) {
-    for(let num of numbers){
-      if (Number(word) === num) {
-        stringNumbers.push(Number(word));
-        break;
-      }
+    if (`${Number(word)}` !== nAn) {
+      stringNumbers.push(Number(word));
     }
   }
   return stringNumbers;
+};
+
+const hydrate = (str) => {
+  let cupsOfWater = 0;
+
+  for (let value of numbersInString(str)) {
+    cupsOfWater += value;
+  }
+  return cupsOfWater > 1 ? `${cupsOfWater} copos de água` : `${cupsOfWater} copo de água`;
 };
 
 module.exports = {
